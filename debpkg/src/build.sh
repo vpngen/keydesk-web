68b6dd249c6e37150542f33103b2a66fc617bda0
Ingmund Olsson <ingmund@proton.me>
2022-12-20 02:13:33 +0200

    save

--- debpkg/src/build.sh
+++ debpkg/src/build.sh
@@ -2,13 +2,17 @@
 
 set -e
 
-cd keydesk-web
-yarn install
-yarn build
+export DEBIAN_FRONTEND=noninteractiv
 
 echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | tee /etc/apt/sources.list.d/goreleaser.list
 apt update
 apt install nfpm
+
+export FORCE_COLOR=false
+
+cd keydesk-web
+yarn install
+yarn build
 
 BUILDDIR=$(mktemp -d)
 
