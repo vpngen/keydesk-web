b073003fdb4e2a44024cb3134a2d2cf89ca9a244
Ingmund Olsson <ingmund@proton.me>
2023-06-09 19:44:01 +0300

    new release

---
+++ src/components/TheLoader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="loader" v-if="isLoading">
+        <div class="loader__overlay welcome">
+        </div>
+        <div class="loader__container">
+            <div class="loader__active-circle"></div>
+            <div class="loader__container-overlay"></div>
+            <div class="loader__content">
+                <div class="loader__logo"></div>
+                <div class="loader__text">
+                    Generating...
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {computed, inject} from "vue";
+
+const loadingService = inject('loadingService');
+const isLoading = computed(() => loadingService.state.isLoading);
+</script>
