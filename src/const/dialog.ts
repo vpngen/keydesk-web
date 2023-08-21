b56163ec0572b8da04a1cf009f139568f105839c
Ingmund Olsson <ingmund@proton.me>
2023-08-21 20:38:00 +0300

    add: add different confs

---
+++ src/const/dialog.ts
@@ -0,0 +1,37 @@
+export const dialogQrDic = [
+	'android',
+	'ios'
+];
+
+export const dialogOsCards = [
+	{
+		label: 'Windows',
+		value: 'windows',
+		link: 'https://bit.ly/WGWin'
+	},
+	{
+		label: 'MacOS',
+		value: 'macos',
+		link: 'https://bit.ly/AmnMac'
+	},
+	{
+		label: 'iOS',
+		value: 'ios',
+		link: 'https://bit.ly/AmnAStore'
+	},
+	{
+		label: 'Android',
+		value: 'android',
+		link: 'https://bit.ly/AmnGPlay'
+	},
+	{
+		label: 'Linux',
+		value: 'linux',
+		link: 'https://bit.ly/AmnNix'
+	},
+	{
+		label: 'Другое',
+		value: 'other',
+		link: ''
+	},
+];
