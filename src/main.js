d8c5a04ff94b8e2b78b2d6e9fc232f3eaf34d4ed
Ingmund Olsson <ingmund@proton.me>
2024-12-18 19:53:48 +0200

    school link

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { createPinia } from 'pinia'
 import loadingService from "@/assets/services/loading-service";
 
 const app = createApp(App);
+const pinia = createPinia()
+
 app.config.globalProperties.window = window
 app.provide('loadingService', loadingService);
+app.use(pinia)
 app.use(router);
 app.mount('#app');
